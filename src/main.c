34661f177edd9ca1cf28eb845b7c8a17f4e3da2d
dioschuarz <diogo.schuarz@gmail.com>
2017-08-07 21:12:16 -0300

    adicionado biblioteca ctype e funções para verificar caracteres especiais

--- src/main.c
+++ src/main.c
@@ -4,28 +4,27 @@
  * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
  * numero de palavras separadas que recebeu e, apos, encerrar.
  */
-
 #include <stdio.h>
-
-int main() {
-
-  char c=0;				// variavel acumuladora de palavras
-  int i=0;
-  int j=0;
-
-  while (c != '\n') {
-    scanf("%c", &c);
-    i++;
-    if(c == ' '){
-	j++;
-	i=0;
-    }
-    if(c == '-'){
-	j++;
-	i=0;
-    }
-  }
-  if(i>0) j++;
-  printf("%d\n",j);
-  return 0;
+#include <ctype.h>
+char c=0,prev=0,prev2=0;
+int count=1;
+int word=0;
+int main (){
+	while(c!='\n'){
+		prev2=prev;
+		prev=c;
+		scanf("%c",&c);
+		if (isalnum(c)){
+		    word=1;
+		}
+		if((isalnum(c))&&((ispunct(prev))||isspace(prev))){ // c=letra ou numero e (and) prev=espaço ou pontução
+		    count++;
+		    if(ispunct(prev)&&isdigit(prev2)&&isdigit(c)) count--;
+		}
+	}
+	if(c=='\n' && !word){
+		count=0;
+	}
+	printf("%d\n",count);
+	return 0;
 }
